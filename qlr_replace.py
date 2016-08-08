3854dcca5a3301f80e70532f787021020ce6f6e3
Anders Barfod <baffyx@gmail.com>
2016-08-08 14:55:00 +0200

    mindre rettelser til print

--- qlr_replace.py
+++ qlr_replace.py
@@ -7,7 +7,7 @@ newhost = 'gc2'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        print (os.path.join(subdir, file))
+        print ('Editing: ' + os.path.join(subdir, file))
         with fileinput.FileInput(os.path.join(subdir, file), inplace=True, backup='.bak') as qlrfile:
             for line in qlrfile:
                 print(line.replace("host=" + oldhost , "host=" + newhost), end='')
