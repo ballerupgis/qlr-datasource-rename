1ac46fb732854b6efc6c623b3a48b4ef94d6fed6
baffioso <andb@ITF400316.balk.dk>
2016-08-09 10:41:00 +0200

    kommenteret koden og fjernet backupfil generering

--- qlr_replace.py
+++ qlr_replace.py
@@ -1,13 +1,20 @@
 import fileinput
 import os
 
+#Angiv rodfolderen for qlr-filerne
 rootdir = 'C:\\andb\\python\\Temastyring'
-oldhost = '172.30.1.161'
-newhost = 'gc2'
+#Angiv den gamle host
+oldhost = 'gc2'
+#Angiv den nye host
+newhost = 'anders'
 
+#Looper over filer i mapper/undermapper
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
+        #For hver fil skrives "Editing: filename" 
         print ('Editing: ' + os.path.join(subdir, file))
-        with fileinput.FileInput(os.path.join(subdir, file), inplace=True, backup='.bak') as qlrfile:
+        with fileinput.FileInput(os.path.join(subdir, file), inplace=True) as qlrfile:
+            #Looper over hver linie i qlr-filerne
             for line in qlrfile:
+                #host erstattes hvis oldhost findes i qlr-filen
                 print(line.replace("host=" + oldhost , "host=" + newhost), end='')
