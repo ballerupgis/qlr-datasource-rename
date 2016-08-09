d3cc715f05ce11cebb015c0a56459613ea9dc029
Josef Assad <josef@josefassad.com>
2016-08-09 12:56:54 +0200

    udskift windows line endings
    Så, et "quality of life" commit. :) Windows og UNIX (herunder linux,
    mac, BSD, etc.) er ikke enige om hvilke tegn man bruger for at
    signallere at en tekstlinje afsluttes. Det fører til at vi i unix
    verdenen ser grimme linjeafslutninger i windowsformatterede filer. Dem
    har jeg fjernet i dette commit.
    
    I princippet gør de ikke noget men jeg tror det som de fleste projekter
    gør når de har contributors fra windows og unix verdenen er, at de
    defaulter til unix line endings og så indstiller alle deres code editors
    så det er standard (det burde man kunne med de fleste windows editors,
    ved ikke hvilken en du bruger).

--- qlr_replace.py
+++ qlr_replace.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import fileinput
 import os
 
 #Angiv rodfolderen for qlr-filerne
 rootdir = 'C:\\andb\\python\\Temastyring'
+rootdir = '/home/josef/tmp/test/'
 #Angiv den gamle host
 oldhost = 'gc2'
 #Angiv den nye host
@@ -13,7 +15,7 @@ newhost = 'anders'
 #Looper over filer i mapper/undermapper
 for subdir, dirs, files in os.walk(rootdir):
     for f in files:
-        #For hver fil skrives "Editing: filename" 
+        #For hver fil skrives "Editing: filename"
         print ('Editing: ' + os.path.join(subdir, f))
         with fileinput.FileInput(os.path.join(subdir, f), inplace=True) as qlrfile:
             #Looper over hver linie i qlr-filerne
