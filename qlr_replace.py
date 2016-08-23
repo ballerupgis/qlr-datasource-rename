11b6ab488850ab706cfefbe6d2058e6ccdc6d46f
Josef Assad <josef@josefassad.com>
2016-08-23 12:55:19 +0200

    små cleanups
    - print() ikke print ()
    - Vi plejer normalt at bruge STORE_BOGSTAVER når der er tale om konstanter.

--- qlr_replace.py
+++ qlr_replace.py
@@ -6,19 +6,19 @@ import os
 import re
 
 #Angiv rodfolderen for qlr-filerne
-rootdir = '/Volumes/DISK_IMG/qlr_dims'
+ROOTDIR = '/Volumes/DISK_IMG/qlr_dims'
 
 #Angiv den nye host
-newhost = 'anders'
+NEWHOST = 'anders'
 
 #Looper over filer i mapper/undermapper
-for subdir, dirs, files in os.walk(rootdir):
+for subdir, dirs, files in os.walk(ROOTDIR):
     for f in files:
         filepath = os.path.join(subdir, f)
         #For hver fil skrives "Editing: filename"
-        print ('Editing: ' + filepath)
+        print('Editing: ' + filepath)
         tree = ET.parse(filepath)
         root = tree.getroot()
         for ds in root.findall('maplayers/maplayer/datasource'):
-            ds.text = re.sub(r'host=[^\s]*', 'host=%s' % newhost, ds.text)
+            ds.text = re.sub(r'host=[^\s]*', 'host=%s' % NEWHOST, ds.text)
         tree.write(filepath)
