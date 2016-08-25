3b31db088ca895326503fc15ec2c59621770330c
Josef Assad <josef@josefassad.com>
2016-08-25 16:47:42 +0200

    mere traditional kodestruktur (closes #7)

--- qlr_replace.py
+++ qlr_replace.py
@@ -6,20 +6,25 @@ import os
 import re
 import sys
 
-#Angiv rodfolderen for qlr-filerne
-ROOTDIR = sys.argv[1]
 
-#Angiv den nye host
-NEWHOST = sys.argv[2]
+def rename_host(ROOTDIR, NEWHOST):
+    #Looper over filer i mapper/undermapper
+    for subdir, dirs, files in os.walk(ROOTDIR):
+        for f in files:
+            filepath = os.path.join(subdir, f)
+            #For hver fil skrives "Editing: filename"
+            print('Editing: ' + filepath)
+            tree = ET.parse(filepath)
+            root = tree.getroot()
+            for ds in root.findall('maplayers/maplayer/datasource'):
+                ds.text = re.sub(r'host=[^\s]*', 'host=%s' % NEWHOST, ds.text)
+            tree.write(filepath)
 
-#Looper over filer i mapper/undermapper
-for subdir, dirs, files in os.walk(ROOTDIR):
-    for f in files:
-        filepath = os.path.join(subdir, f)
-        #For hver fil skrives "Editing: filename"
-        print('Editing: ' + filepath)
-        tree = ET.parse(filepath)
-        root = tree.getroot()
-        for ds in root.findall('maplayers/maplayer/datasource'):
-            ds.text = re.sub(r'host=[^\s]*', 'host=%s' % NEWHOST, ds.text)
-        tree.write(filepath)
+if __name__ == '__main__':
+    #Angiv rodfolderen for qlr-filerne
+    ROOTDIR = sys.argv[1]
+
+    #Angiv den nye host
+    NEWHOST = sys.argv[2]
+
+    rename_host(ROOTDIR, NEWHOST)
